67d067aaba5f334f16710af42417ac2d4786b7b5
Frank G <s1226135@monmouth.edu>
2020-02-04 20:29:13 -0500

    Part 2 commit
    Change-Id: I6ea838925ff062638cb1a9179b8c9710cce56dd3

--- src/Project.java
+++ src/Project.java
@@ -4,32 +4,71 @@ public class Project {
 	public static void main(String[] args) {
 
 		Scanner in = new Scanner(System.in);
+		
+		
+
+
+		
+
+
+	
+
+	int exitLoop = 1;
+	while (exitLoop == 1)
+	{
 		System.out.println("How much have you spent today?");
+		if (in.hasNextDouble())
+		{
 
+			double amountSpent = in.nextDouble();
+			if (amountSpent == 0) 
+			{
+				exitLoop = 0;
+			}
+			double percentDiscount = 0;
+			
+			if (amountSpent < 10 || amountSpent > 1000)
+			
+			{
+				//NO DISCOUNT
 
-
-		double amountSpent = in.nextDouble();
-		double percentDiscount = 0;
-		if (amountSpent < 10) {
-			//NO DISCOUNT
-		} else if (amountSpent <= 60) {
-			percentDiscount = 0.08;
-		} else if (amountSpent <= 150) {
-			percentDiscount = 0.1;
-		} else if (amountSpent <= 210) {
-			percentDiscount = 0.12;
-		} else if (amountSpent >= 210.01) {
-			percentDiscount = 0.14;
+			} 
+			else if (amountSpent <= 60)
+			{
+				percentDiscount = 0.08;
+			} 
+			else if (amountSpent <= 150)
+			{
+				percentDiscount = 0.1;
+			} 
+			else if (amountSpent <= 210)
+			{
+				percentDiscount = 0.12;
+			} 
+			else if (amountSpent >= 210.01)
+			{
+				percentDiscount = 0.14;
+			}
+			double moneyOff = amountSpent * percentDiscount;
+			System.out.printf("You win a discount coupon of $%.2f", moneyOff);
+			System.out.println();
 		}
-		double moneyOff = amountSpent * percentDiscount;
-		System.out.printf("You win a discount coupon of $%.2f", moneyOff);
-		//System.out.println(amountSpent);
-		//System.out.println(percentDiscount);
-
-
-
-
+		else 
+		{
+			System.out.println(in.next()+ " is not a valid input");
+		}
 	}
+
+
+
+
+
+
+
+
+
+
+}
 
 
 
